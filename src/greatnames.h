191b48a6f19e239e48d86fc521490133e40f7637
JamesNewton <jamesnewton@massmind.org>
2022-04-06 16:08:29 -0700

    TODO: Fill this out the rest of the way

---
+++ src/greatnames.h
@@ -0,0 +1,40 @@
+//a list of names of people who changed computing, with careful emphasis on the first of each thing. E.g.
+//First assmbly language: Kathleen Booth
+//First hacker: Ada Lovelace
+//Solved the routing problem and made the internet as we know it possible: Radia Joy Perlman
+//but also including men who made significant firsts:
+//First programmable computer: Konrad Zuse
+//First compiler: Alick Edwards Glennie
+
+const char robot1[] PROGMEM = "HelenChanWolf";
+const char robot2[] PROGMEM = "GraceHopper";
+const char robot3[] PROGMEM = "MargaretHamilton";
+const char robot4[] PROGMEM = "JosephMarieJacquard";
+const char robot5[] PROGMEM = "AdaLovelace";
+const char robot6[] PROGMEM = "KathleenHyldaValerieBooth";
+const char robot7[] PROGMEM = "JohnWarnerBackus";
+const char robot8[] PROGMEM = "JohnMcCarthy";
+const char robot9[] PROGMEM = "TimBerners-Lee";
+const char robot10[] PROGMEM = "RadiaJoyPerlman";
+const char robot11[] PROGMEM = "SallyJeanFloyd";
+const char robot12[] PROGMEM = "ElizabethJocelyFeinler";
+const char robot13[] PROGMEM = "AlanCurtisKay";
+const char robot14[] PROGMEM = "DanielHenryHolmesIngallsJr";
+const char robot15[] PROGMEM = "AdeleGoldberg";
+const char robot16[] PROGMEM = "TedKaehler";
+const char robot17[] PROGMEM = "DianaMerry-Shapiro";
+const char robot18[] PROGMEM = "BrianWKernighan";
+const char robot19[] PROGMEM = "DennisMRitchie";
+const char robot20[] PROGMEM = "Nicole-ReineLepaute";
+const char robot21[] PROGMEM = "MariaMitchell";
+const char robot22[] PROGMEM = "MaryClareCoombs";
+const char robot23[] PROGMEM = "KaterynaLohvynivnaYushchenko";
+const char robot24[] PROGMEM = "AdeleMildredKoss";
+const char robot25[] PROGMEM = "MaryKennethKeller";
+const char robot26[] PROGMEM = "JohnGKemeny";
+const char robot27[] PROGMEM = "ThomasEKurtz";
+const char robot28[] PROGMEM = "AlanMathisonTuring";
+const char robot29[] PROGMEM = "KonradErnstOttoZuse";
+const char robot30[] PROGMEM = "AlickEdwardsGlennie";
+const char robot31[] PROGMEM = "";
+const char robot32[] PROGMEM = "";
